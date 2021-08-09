f8c25747c39c472ebbdd8192f8ec1660f725977f
ZumTheZazaKing <zahidi85543@gmail.com>
2021-08-09 16:59:18 +0800

    Functions added. Time for design

--- src/index.js
+++ src/index.js
@@ -1,27 +1,70 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import reportWebVitals from './reportWebVitals';
+import { loremIpsum } from 'lorem-ipsum';
 
-function Input(){
+function Input(props){
   return <div>
-    <h2>Metaphorpsum Generator</h2>
-    Paragraphs<input type="number" max={99}/><br/>
-    Sentences<input type="number" max={99}/>
+    <h2>Lorem Ipsum Generator</h2>
+    <form onSubmit={e => props.generate(e)}>
+      Paragraphs: <input type="number" min={1} max={99} value={props.paragraphs} onChange={e => props.changeParagraphs(e)} required/>
+      <br/>
+      Sentences per Paragraph: <input type="number" min={1} max={99} value={props.sentences} onChange={e => props.changeSentences(e)}/>
+      <br/>
+      Words per Sentence: <input type="number" min={1} max={99} value={props.words} onChange={e => props.changeWords(e)}/>
+      <br/>
+      <input type="submit" value="Generate"/>
+    </form>
   </div>
 }
 
-function Output(){
-  return <div>
-    
-  </div>
+function Output(props){
+  return <div dangerouslySetInnerHTML={{__html:props.generatedText}}></div>
 }
 
 function App(){
 
+  let [paragraphs, setParagraphs] = useState("");
+  const changeParagraphs = e => setParagraphs(e.target.value); 
+  let [sentences, setSentences] = useState("");
+  const changeSentences = e => setSentences(e.target.value);
+  let[words, setWords] = useState("");
+  const changeWords = e => setWords(e.target.value);
+
+  let [generatedText, setGeneratedText] = useState("");
+
+  function generate(e){
+    e.preventDefault();
+
+
+    setGeneratedText(loremIpsum({
+      count:parseInt(paragraphs),
+      format:"html",
+      paragraphLowerBound: sentences ? parseInt(sentences) : 3,
+      paragraphUpperBound: sentences ? parseInt(sentences) : 7,
+      sentenceLowerBound: words ? parseInt(words) : 5,   // Min. number of words per sentence.
+      sentenceUpperBound: words ? parseInt(words) : 15,  // Max. number of words per sentence.
+      units:"paragraphs",
+      suffix:"\n\n"
+    }));
+
+  }
+
   return <div>
-    <Input/>
-    <Output/>
+    <Input 
+    paragraphs={paragraphs} 
+    sentences={sentences}
+    words={words}
+    changeParagraphs={changeParagraphs} 
+    changeSentences={changeSentences}
+    changeWords={changeWords}
+    generate={generate}
+    />
+
+    <Output
+    generatedText={generatedText}
+    />
   </div>
   
 }
